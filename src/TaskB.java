ea5fd48971a77fb5d404511aec643163478711b4
byval <byvaltsev1978@gmail.com>
2023-10-23 23:07:58 +0300

    Create main method in TaskB class

--- src/TaskB.java
+++ src/TaskB.java
@@ -1,3 +1,9 @@
-public class TaskB {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class TaskB {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
